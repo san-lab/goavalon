052756aefeaec9eba83a674dccff0588f6246438
kubasiemion <kuba.siemion@gmail.com>
2020-07-14 11:22:38 +0200

    sample python ed generation

--- ec-signature-verify.py
+++ ec-signature-verify.py
@@ -32,17 +32,34 @@ def verify(message, pbkey, signature):
 
     Rp = cv.decode_point(R)
     Pb = cv.decode_point(keybytes)
-
     X = cv.add_point(Rp, cv.mul_point(hint, Pb))
+    # R + hash(R+m)Pb == sGoh sure sure
     return (X.eq(S))
 
+
+def generateFromSeed(seed):
+#TODO verify that the seed is 32 bytes
+    h = hashlib.sha512()
+    h.update(seed)
+    dig64 = h.digest()
+
+    #clamp
+
+    dig64[0] &= 248
+	dig64[31] &= 127
+	dig64[31] |= 64
+
+	dint = int.from_bytes(dig64[:32], "big")
+
+	pubPoint = cv.mul_point(dint,g)
+	privkey = seed + cv.encode_point(pubPoint)
 
 
 
 
 IssuerPublicKey = "6cc0580343356515c288897c68dad03a2063d1ea9c03c14af40174bef52d1503"
 IssuerSignature = "acd34a616765820fe6c57ff1ddcbcbed55667e141b99aa2e86f723e16060407a260bc1b4e61e0d763f025550ede2f65e2f87d348adfc9da9b81ded7d33f62008"
-Message =  "Przemek"+"333"+"Average account balance cert"+"25000 EUR"+"0042"
+Message =  "Przemek"+"333"+"Average account balance cert"+"25001 EUR"+"0042"
 
 v = verify(Message, IssuerPublicKey, IssuerSignature)
 print("Signature valid:", v)
